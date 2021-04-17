ab8ca4bc748b79bac3a140c21ace1f17b0f235d2
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-17 08:00:21 +0530

    java test commit

---
+++ Selenium-Gurushools-maven/src/test/java/rediff/testcases/JavaTest.java
@@ -0,0 +1,16 @@
+package rediff.testcases;
+
+public class JavaTest {
+
+	public JavaTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello world test");
+		System.out.println("new change");
+	}
+
+}
